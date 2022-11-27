20d2bfbfccc1cddb4bca03e62074106d9a2cfb64
heodohyeong <jackt5000@naver.com>
2022-11-27 14:52:44 +0900

    spring security - Async

--- src/main/java/com/example/springbootsecurity/form/SampleController.java
+++ src/main/java/com/example/springbootsecurity/form/SampleController.java
@@ -2,12 +2,16 @@ package com.example.springbootsecurity.form;
 
 import com.example.springbootsecurity.account.AccountContext;
 import com.example.springbootsecurity.account.AccountRepository;
+import com.example.springbootsecurity.common.SecurityLogger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.security.Principal;
+import java.util.concurrent.Callable;
 
 @Controller
 public class SampleController {
@@ -53,5 +57,18 @@ public class SampleController {
     public String user(Model model , Principal principal){
         model.addAttribute("message","Hello user" + principal.getName());
         return "user";
+    }
+
+    @GetMapping("/async-handler")
+    @ResponseBody
+    public Callable<String> asyncHandler(){
+        SecurityLogger.log("MVC");
+        return new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                SecurityLogger.log("Callable");
+                return "Async Handler";
+            }
+        };
     }
 }
